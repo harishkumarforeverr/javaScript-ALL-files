641895861d24da63a452c1205c34bc26413078af
harishkumar809 <harishking809@gmail.com>
2020-12-28 20:27:02 +0530

    Add files via upload

---
+++ workshop/book record/1.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	</head>
+	<body>
+		<header>
+			<meta http-equiv="refresh", content="2,ggchomepage.php" /> 
+		</header>
+		      <section>
+      		    <h1> WELCOME TO :) </h1>
+		      	<h1> GIRRAJ GOVT DEGREE COLLEGE, NIZAMABAD </h1>
+			  </section>
+		<footer>
+			
+		</footer>
+	</body>
+</html>
